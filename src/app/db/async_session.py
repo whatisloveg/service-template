be01b9479f73ae9fb4b79659c48149bef361bcb6
vladislav.g <sdive1337@gmail.com>
2025-11-27 01:47:29 +0300

    удалил выведение логов БД транзакций

--- src/app/db/async_session.py
+++ src/app/db/async_session.py
@@ -8,7 +8,7 @@ from app.core.settings import config
 
 
 async_engine = create_async_engine(
-    str(config.db_cfg.SQLALCHEMY_DATABASE_URI), echo=True, pool_pre_ping=True
+    str(config.db_cfg.SQLALCHEMY_DATABASE_URI), echo=False, pool_pre_ping=True
 )
 async_session = async_sessionmaker(
     autocommit=False, autoflush=False, bind=async_engine, expire_on_commit=False
